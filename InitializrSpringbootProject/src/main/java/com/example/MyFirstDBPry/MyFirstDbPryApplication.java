0af52349163514b9f779552831a5a6f6542b33ca
cositas <@gmail.com>
2026-02-18 08:35:08 -0500

    first commit

---
+++ InitializrSpringbootProject/src/main/java/com/example/MyFirstDBPry/MyFirstDbPryApplication.java
@@ -0,0 +1,19 @@
+package com.example.MyFirstDBPry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MyFirstDbPryApplication {
+    @RequestMapping("/")
+	String home() {
+	return "Juan Felipe Guzman Rueda";
+    }
+    public static void main(String[] args) {
+            SpringApplication.run(MyFirstDbPryApplication.class, args);
+    }
+
+}
